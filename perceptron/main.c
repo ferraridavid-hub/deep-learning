d9dd0a71f5f2994b5a58cd9edd90a180619bc9ce
david_ferrari <ferraridavid.hub@gmail.com>
2025-07-15 00:51:51 +0200

    Trying to test Perceptron on the MNIST dataset
    - reading the dataset from a txt file with long lines of space-separated
      doubles.

--- perceptron/main.c
+++ perceptron/main.c
@@ -1,58 +1,94 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #include "perceptron.h"
 #include "training_set.h"
 
-
 #define TRAINING_SET_SIZE 100000000
 
+void init_random_training_set(TrainingSet *ts)
+{
 
-void init_random_training_set(TrainingSet* ts, int seed) {
-   srand(seed); 
-   ts->input = (double**)malloc(sizeof(double*) * TRAINING_SET_SIZE);
-   for (size_t i = 0; i < TRAINING_SET_SIZE; i++) {
-    ts->input[i] = (double*)malloc(sizeof(double) * 2);
-   }
+    double **input = (double **) malloc(sizeof(double) * 256);
+    bool **label = (bool **)malloc(sizeof(bool*));
+    label[0] = (bool *) malloc(sizeof(bool) * 256);
 
-   ts->labels = (bool**)malloc(sizeof(bool*) * 2);
-   for (size_t i = 0; i < 2; i++) {
-    ts->labels[i] = (bool*) malloc(sizeof(bool) * TRAINING_SET_SIZE);
-   }
+    size_t i = 0;
+    size_t m_cap = 256;
 
-   ts->n_features = 2;
-   ts->n_labels = 2;
-   ts->size = TRAINING_SET_SIZE;
+    FILE *fp = fopen("data/nmist_5.txt", "r");
+    char *line = NULL;
+    size_t n = 0;
 
-   for (size_t i = 0; i < TRAINING_SET_SIZE; i++) {
-       double r = (double)rand() / RAND_MAX * 20;
+    while (getline(&line, &n, fp) != -1)
+    {
 
-       double theta = (double)rand() / RAND_MAX * 2 * M_PI;
+        input[i] = (double *)malloc(sizeof(double) * 256);
 
-       //ts->input[i][0] = r * sin(theta);
-       //ts->input[i][1] = r * cos(theta);
-       ts->input[i][0] = r;
-       ts->input[i][1] = theta;
+        size_t k = 0;
+        size_t cap = 256;
 
-       ts->labels[0][i] = (r < 5); 
-       ts->labels[1][i] = (r >= 10);
-   }
+        char *ptr = line;
+        for (;;)
+        {
+            char *prev = ptr;
+            double d = strtod(ptr, &ptr);
+            if (prev == ptr)
+            {
+                if (k > 0)
+                {
+                    label[0][i] = input[i][--k];
+                }
+                break;
+            }
+
+            input[i][k++] = d;
+
+            if (k == cap)
+            {
+                cap *= 2;
+                input[i] = realloc(input[i], cap * sizeof(double));
+            }
+
+        }
+
+        ts->n_features = k;
+        ts->n_labels = 1;
+
+        i++;
+        
+        if (i == m_cap) {
+            m_cap *= 2;
+            input = realloc(input, m_cap * sizeof(double *));
+            label[0] = realloc(label[0], m_cap * sizeof(bool));
+        }
+
+        free(line);
+        line = NULL;
+    }
+    free(line);
+
+    ts->input = input;
+    ts->labels = label;
+    ts->size = i;
 }
 
+int main(int argc, char *argv[])
+{
+     if (argc < 2) {
+         fprintf(stderr, "usage: perceptron grade\n");
+         exit(1);
+     }
+
+     TrainingSet training_set;
+     init_random_training_set(&training_set);
 
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "usage: perceptron grade\n");
-        exit(1);
-    }
+    //  print_training_set(&training_set);
 
-    TrainingSet training_set;
-    init_random_training_set(&training_set, 42);
-    
-    // print_training_set(&training_set);
 
     Perceptron perceptron;
     init_perceptron(&perceptron);
@@ -61,32 +97,35 @@ int main(int argc, char *argv[]) {
 
     free_training_set(&training_set);
 
-    bool** prevs = (bool**) malloc (sizeof(bool*) * (argc - 1) / 2);
-    int k = 0;
+    print_perceptron(&perceptron);
 
-    while (argc > 1) {
+     argv++;
 
-        double x = atof(*(++argv));
-        double y = atof(*(++argv));
-        double input[] = {x, y};
-        bool* passed = predict_perceptron(&perceptron, input);
-        prevs[k++] = passed;
+    // bool** prevs = (bool**) malloc (sizeof(bool*) * (argc - 1) / 2);
+    // int k = 0;
 
-        argc -= 2;
-    }
+    // while (argc > 1) {
 
-    printf("Previsions (k=%d):\n", k);
-    printf("==================================\n");
-    for (int i = 0; i < k; i++) {
-        printf("label1: %d\tlabel2: %d", prevs[i][0], prevs[i][1]);
-        putchar('\n');
-        free(prevs[i]);
-    }
+    //     double x = atof(*(++argv));
+    //     double y = atof(*(++argv));
+    //     double input[] = {x, y};
+    //     bool* passed = predict_perceptron(&perceptron, input);
+    //     prevs[k++] = passed;
 
-    free(prevs);
+    //     argc -= 2;
+    // }
+
+    // printf("Previsions (k=%d):\n", k);
+    // printf("==================================\n");
+    // for (int i = 0; i < k; i++) {
+    //     printf("label1: %d\tlabel2: %d", prevs[i][0], prevs[i][1]);
+    //     putchar('\n');
+    //     free(prevs[i]);
+    // }
+
+    // free(prevs);
 
     free_perceptron(&perceptron);
 
     return EXIT_SUCCESS;
-
 }
