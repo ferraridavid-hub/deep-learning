d1e8b46f0f2181d30e84426aa3978192392a2ca1
david_ferrari <ferraridavid.hub@gmail.com>
2025-07-18 15:28:12 +0200

    Matrix initialization with random numbers.

--- matrix/main.c
+++ matrix/main.c
@@ -7,12 +7,8 @@
 
 int main() {
     Matrix m;
-    mat_init(&m, 28 * 28, 28 * 28);
-    m.items[0] = 1;
-    m.items[1] = 2;
-    m.items[2] = 3;
-    m.items[3] = 4;
-    //matprint(&m);
+    mat_init_random(&m, 2, 2);
+    matprint(&m);
 
     struct timeval start;
     gettimeofday(&start, NULL);
@@ -23,14 +19,10 @@ int main() {
     uint64_t us_start = start.tv_usec + start.tv_sec * 1000000;
     uint64_t us_end = end.tv_usec + end.tv_sec * 1000000;
 
-    //matprint(c);
+    matprint(c);
 
     printf("difftime: %zu\n", us_end - us_start);
 
-
     matfree(&m);
     matfree(c);
-
-    free(c);
-
 }
